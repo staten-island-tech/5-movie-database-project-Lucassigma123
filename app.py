bddfda8d9c4429dbb70b5e168997869355811e40
Lucassigma123 <lucass126@nycstudents.net>
2025-10-21 10:17:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-
-x =input("write a year and i will show all movies created in that year")
-print(
+print (data)
+x =(input("write a year and I will tell all movies created in that year"))
+print(x(data))
